33cebaff9025d5a6bce5d80877d7db0c397dd2ed
hyewon <w2106@e-mirim.hs.kr>
2023-03-16 15:05:46 +0900

    callback, 비동기

--- callback.js
+++ callback.js
@@ -5,3 +5,15 @@ function somefunc(callback) {
   //callback()가 실행된다.
   callback();
 }
+
+function cb() {
+  console.log("콜백함수 실행");
+}
+
+somefunc(function () {
+  console.log("콜백함수 실행");
+});
+
+//비동기 : 순서대로 실행되지 않는 것. 자바스크립트는 비동기 방식으로 실행된다.
+//콜백 함수 : 자바스크립트에서 순서대로 실행되어야 하는 부분이 있는 경우 콜백 함수 사용.
+
