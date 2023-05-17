2956281a2fe0dedd9a738ce11fab46cc55ae7257
hyewon <w2106@e-mirim.hs.kr>
2023-05-17 10:00:20 +0900

    ejs test

---
+++ grace/template.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const ejs = require('ejs');
+
+//index.ejs에 넘겨줄 data
+const index = {
+  name: 'YHW',
+  here: '한국'
+};
+
+const server = http.createServer(function(req, res){
+  console.log(req.url);
+  
+  res.setHeader('Content-type', 'text/html');
+  if(req.url == '/') {
+    ejs.renderFile(path.join(__dirname, 'template', './index.ejs'), 
+    {name: index.name, here: index.here},
+    //데이터를 넘겨받은 ejs코드(data)를 클라이언트에게 보낸다.
+    function(err, data) {
+      res.end(data);
+    })
+
+  }
+  else if(req.url == '/food') {
+    fs.createReadStream(path.join(__dirname, 'html', './food.html')).pipe(res);
+  }
+  else {
+    fs.createReadStream(path.join(__dirname, 'html', './404.html')).pipe(res);
+  }
+
+});
+
+server.listen(3333);
